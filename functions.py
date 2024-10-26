535ee4c79721b3c1c59baa022cbd9bfa9648ea55
Rodrigo Miranda <rucamir2004@gmail.com>
2024-10-26 12:16:37 +0100

    more functions

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import math
+
 class StockManager:
     def __init__(self):
         self.initial_stock = 2200
@@ -6,12 +8,20 @@ class StockManager:
         self.posession_cost = 0.7
         self.delivery_time = 7
         self.security_stock = 1000
+        self.daily_demand = self.anual_demand/365
 
         self.material_to_number = {
             'Tecido': 0,
             'Algodao': 1,
             'Fio': 2,
             'Poliester': 3
+        }
+
+        self.number_to_material = {
+            0: 'Tecido',
+            1: 'Algodao',
+            2: 'Fio',
+            3: 'Poliester'
         }
 
         self.clothing_sizes_price = {
@@ -57,12 +67,34 @@ class StockManager:
             4: 'XL'
         }
 
+        self.price_per_square_meter = {
+            self.material_to_number['Tecido']: 7,
+            self.material_to_number['Algodao']: 5.5,
+            self.material_to_number['Fio']: 4.5,
+            self.material_to_number['Poliester']: 10
+        }
+
+        self.stock_levels = {
+            self.material_to_number['Tecido']: 2200,
+            self.material_to_number['Algodao']: 2200,
+            self.material_to_number['Fio']: 2200,
+            self.material_to_number['Poliester']: 2200,
+        }
+
+    def get_stock(self, material):
+        return self.stock_levels[self.number_to_material[material]]
+
     def order_point(self, material):
         if material not in self.material_to_number:
             return "Material desconhecido."
         
-        material_num = self.material_to_number[material]
-        return f"Ponto de encomenda calculado para o material {material_num}"
+        orderPoint = round((self.daily_demand * self.delivery_time) + self.security_stock)
+        return orderPoint
+    
+    def enconomic_order_quantity(self):
+        return round(math.sqrt((2*self.anual_demand*self.order_cost)/self.posession_cost))
+        
 
 manager = StockManager()
+print(manager.enconomic_order_quantity())
 print(manager.order_point('Tecido'))
