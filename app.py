fa28dc03f1ec1a90d08320597ec44efeb34c8270
Filipe Correia <68955432+fmcorreia04@users.noreply.github.com>
2024-10-26 12:57:52 +0100

    improved app

--- app.py
+++ app.py
@@ -2,31 +2,64 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-# Home route
+
+PRODUCTS = ["T-Shirts", "Calcões", "Camisolas", "Calças"]
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', products=PRODUCTS)
 
-# About route
-@app.route('/about')
+@app.route('/status')
 def about():
-    return render_template('about.html')
+    return render_template('status.html')
 
+
+def json(message):
+    return jsonify({'message': message})
 
 
 @app.route('/api/buy', methods=['POST'])
 def api_buy():
-    # Retrieve data from the form submission
-    name = request.form.get('name')
+    
     product = request.form.get('product')
     quantity = request.form.get('quantity')
+
+    if  not product or not quantity:
+        return json('Please provide all required fields'), 400
+
+    if quantity <= 0:
+        return json('Invalid'), 400
     
-    # Check if required fields are provided
-    if not name or not product or not quantity:
-        return jsonify({'error': 'Please provide all required fields'}), 400
+
+    # here call functions by rodrigo
 
 
 
+    response = {
+        'message': f'Your order for {quantity}x{product} has been received.',
+    }
+
+    return jsonify(response), 200
+
+
+
+
+@app.route('/api/buyformat', methods=['POST'])
+def api_buy():
+    
+    data = request.form.get('data')
+
+    if  not data:
+        return json('Please provide all required fields'), 400
+
+
+    # here call functions by rodrigo
+
+    response = {
+        'message': f'Your order has been received.',
+    }
+
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
